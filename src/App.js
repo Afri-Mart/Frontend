4c6f57cd326345c932fcc379831e7659cce31d8f
KOSSY <KosyAbone>
2022-03-14 21:39:42 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App () {
           {/*<SearchIcon />*/}
           <Header />
           <NavBar/>
-          <LoginForm/>
+          {/* <LoginForm/> */}
       </BrowserRouter>
     )
     
