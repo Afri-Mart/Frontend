555b05cced5093574873598340719e746e05c078
KOSSY <KosyAbone>
2022-03-17 13:10:32 +0100

    added product display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 //import { Link } from 'react-router-dom';
-import LoginForm from './components/Login/login'
+//import LoginForm from './components/Login/login'
 import Header from './components/Header/Header';
 import NavBar from './components/Header/NavBar';
+import Products from './components/Products/products';
 function App () {
 
     return (
@@ -12,6 +13,7 @@ function App () {
           <Header />
           <NavBar/>
           {/* <LoginForm/> */}
+          <Products />
       </BrowserRouter>
     )
     
---
+++ src/components/Products/products.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Product from './product'
+import './product.css'
+
+function Products() {
+
+    const [items, setItems] = useState([]);
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        const getData = async () => {
+            try{
+                const url = 'https://afri-mart.herokuapp.com/api/items'
+                const res = await fetch(url)
+                //const result = await axios.get(url)
+                const result = await res.json()
+                console.log(result.data)
+                setItems(result.data)
+            }catch(err){
+                console.log(err)
+                setError(err)
+            }
+        }
+
+        getData()
+    }, [])
+
+    const fetchedData = items.map(each => 
+        <Product 
+            key={each.id} 
+            title={each.Title} 
+            price={each.Price}
+            image={each.image}
+        />)
+     
+  return (
+    <div className='item-list'>
+       {fetchedData}
+    </div>
+  )
+}
+
+export default Products
