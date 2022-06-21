4074a0ce5cf061a2f88f865c20b9333e76520af2
KOSSY <abonekosiso@gmail.com>
2022-06-21 16:30:56 +0100

    h

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import Home from './components/Pages/Home/home'
 // import Login from './components/Pages/Login/login'
 
 function App () {
-
     return (
-      
       <div>
          {/* <Login /> */}
           <Home />
       </div>
       
     )
-    
 }
 
 export default App
